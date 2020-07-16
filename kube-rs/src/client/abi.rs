85681a74fbedb80c67b95b06ae85aab64e0dd890
slinkydeveloper <francescoguard@gmail.com>
2020-07-16 15:18:20 +0200

    Another change
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- kube-rs/src/client/abi.rs
+++ kube-rs/src/client/abi.rs
@@ -1,9 +1,9 @@
 // This program will be compiled to WebAssembly using a Custom ABI
 // We will then use Wasmer embedded to run this module.
-use std::{str, mem};
+use safe_transmute::{transmute_one, transmute_to_bytes};
+use serde::{Deserialize, Serialize};
 use std::ffi::c_void;
-use safe_transmute::{transmute_to_bytes, transmute_one};
-use serde::{Serialize, Deserialize};
+use std::{mem, str};
 
 // Define the functions that this module will use from the outside world.
 // In general, the set of this functions is what we define as an ABI.
@@ -36,8 +36,7 @@ unsafe impl safe_transmute::TriviallyTransmutable for Ptr {}
 
 impl From<u64> for Ptr {
     fn from(value: u64) -> Self {
-        transmute_one(transmute_to_bytes(&[value]))
-            .unwrap()
+        transmute_one(transmute_to_bytes(&[value])).unwrap()
     }
 }
 
@@ -53,7 +52,7 @@ struct HttpRequest {
     #[serde(with = "http_serde::header_map")]
     headers: http::HeaderMap,
 
-    body: Vec<u8>
+    body: Vec<u8>,
 }
 
 impl From<http::Request<Vec<u8>>> for HttpRequest {
@@ -64,7 +63,7 @@ impl From<http::Request<Vec<u8>>> for HttpRequest {
             method: parts.method,
             uri: parts.uri,
             headers: parts.headers,
-            body
+            body,
         }
     }
 }
@@ -77,13 +76,12 @@ struct HttpResponse {
     #[serde(with = "http_serde::header_map")]
     headers: http::HeaderMap,
 
-    body: Vec<u8>
+    body: Vec<u8>,
 }
 
 impl Into<http::Response<Vec<u8>>> for HttpResponse {
     fn into(self) -> http::Response<Vec<u8>> {
-        let mut builder = http::response::Builder::new()
-            .status(self.status_code);
+        let mut builder = http::response::Builder::new().status(self.status_code);
 
         for (h, v) in self.headers.iter() {
             builder = builder.header(h, v);
@@ -97,10 +95,16 @@ pub fn execute_request(req: http::Request<Vec<u8>>) -> http::Response<Vec<u8>> {
     let inner_request: HttpRequest = req.into();
     let bytes = bincode::serialize(&inner_request).unwrap();
 
-    let response_ptr: Ptr = unsafe { request(bytes.as_ptr(), bytes.len(), allocate as usize as u32) }
-        .into();
+    let response_ptr: Ptr =
+        unsafe { request(bytes.as_ptr(), bytes.len(), allocate as usize as u32) }.into();
 
-    let response_raw = unsafe { Vec::from_raw_parts(response_ptr.ptr as *mut u8, response_ptr.size as usize, response_ptr.size as usize) };
+    let response_raw = unsafe {
+        Vec::from_raw_parts(
+            response_ptr.ptr as *mut u8,
+            response_ptr.size as usize,
+            response_ptr.size as usize,
+        )
+    };
     let response_inner: HttpResponse = bincode::deserialize(&response_raw).unwrap();
 
     response_inner.into()
